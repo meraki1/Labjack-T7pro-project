24298d7a469e3726409c371b6e2e8db68e1f90de
meraki1 <karlo123miskovic@gmail.com>
2023-12-30 22:02:56 +0100

    Defined and installed first-hand dependencies for frontend folder for Typescript React.js project.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PreExperimentView from './views/PreExperimentView';
+import DataCollectionView from './views/DataCollectionView';
+import PostExperimentView from './views/PostExperimentView';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Render your views here */}
+      <PreExperimentView />
+      <DataCollectionView />
+      <PostExperimentView />
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/src/views/PreExperimentView.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface PreExperimentViewProps {
+  // Define the props for this component
+}
+
+const PreExperimentView: React.FC<PreExperimentViewProps> = (props) => {
+  return (
+    <div>
+      {/* Your form goes here */}
+    </div>
+  );
+};
+
+export default PreExperimentView;
